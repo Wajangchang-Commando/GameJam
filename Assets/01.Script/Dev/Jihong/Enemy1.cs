e5e7a7b1aa1229276ce54420e507df798ec13052
phjh <adsf8800@naver.com>
2022-07-12 01:45:56 +0900

    enemy
    적 움직임,쏘기

---
+++ Assets/01.Script/Dev/Jihong/Enemy1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    //»¡°£´« ÀûÀÇ ¿òÁ÷ÀÓ/ ÃÑ¾Ë»ç
+    [SerializeField]
+    GameObject Enemy_bullet;
+    float currenttime;
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, Random.Range(-2.4f,3.5f), transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currenttime +=Time.deltaTime;
+        if(currenttime > 1)
+        {
+            GameObject Bullet = Instantiate(Enemy_bullet);
+            Bullet.transform.position = transform.position;
+            currenttime = 0;
+        }
+    }
+}
