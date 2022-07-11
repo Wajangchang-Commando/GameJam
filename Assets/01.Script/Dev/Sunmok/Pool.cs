15ca3fcf7ac72af60d0cedff1306abb6dff1a4b2
ban324 <ban324@naver.com>
2022-07-12 01:18:43 +0900

    FullManager
    풀관리자

--- Assets/01.Script/Dev/Sunmok/Pool.cs
+++ Assets/01.Script/Dev/Sunmok/Pool.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Pool : MonoBehaviour
 {
-    public Stack<Poolable> pool;
+    public Stack<Poolable> pool = new Stack<Poolable>();
 
-    /*public Poolable push(Poolable obj)
+    public void Create(Poolable obj)
+    {
+        for(int i = 0; i< 5; i++)
+        {
+            Poolable obk = Instantiate(obj);
+            obk.name = obj.name;
+            pool.Push(obk);
+            obk.gameObject.SetActive(false);
+            
+        }
+    }
+    public Poolable push(Poolable obj)
     {
         pool.Push(obj);
         obj.gameObject.SetActive(false);
+        return obj;
     }
     public Poolable pop()
     {
-        Poolable obj = pool.Pop;
-    }*/
+        Poolable obj = pool.Pop();
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
 }
--- Assets/01.Script/Dev/Sunmok/PoolManager.cs
+++ Assets/01.Script/Dev/Sunmok/PoolManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Dictionary<string, Pool> pools = new Dictionary<string, Pool>();
+    //poolable들로 추가할 것
+    public Poolable obj1;
+    public Poolable obj2;
+    public Poolable obj3;
+    public static PoolManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            make(obj1); ;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Summon(obj1.name);
+        }
+    }
+    private void make(Poolable pol)
+    {
+        Pool pool = new Pool();
+        pool.Create(pol);
+        pools.Add(pol.name, pool);
+    }
+    private Poolable Summon(string name)
+    {
+        Poolable obj = pools[name].pop();
+        return obj;
+    }
+    private void Returner(Poolable obj)
+    {
+        pools[obj.name].push(obj);
     }
 }
