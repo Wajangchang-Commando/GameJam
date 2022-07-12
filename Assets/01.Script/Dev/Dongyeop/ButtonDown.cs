85dbc295a9455a5cfafc771525ae0ab21052682c
Dongyeop <kitty-777@naver.com>
2022-07-12 10:50:21 +0900

    일부 버튼 Sprite추가, 만든거 프리팹으로 변경

--- Assets/01.Script/Dev/Dongyeop/ButtonDown.cs
+++ Assets/01.Script/Dev/Dongyeop/ButtonDown.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonDown : MonoBehaviour // 버튼 눌렀을때 사용되는 스크립트에용 ㅎㅎ
 {
+    [SerializeField] private int _sceneScore1;
+
     public void ExitButtonDown()
     {
         Time.timeScale = 1;
         Application.Quit();
+    }
+
+    public void SceneChanger1()
+    {
+        SceneManager.LoadScene(_sceneScore1);
     }
 }
